c45a4483231fb075d948be0599ea9954535d1cfc
BrendonSmith <81374445+Brendon85@users.noreply.github.com>
2024-12-09 20:14:54 +0800

    Update server.lua
    changed mysql.asinc to oxmysql

--- rdn_companions/server/server.lua
+++ rdn_companions/server/server.lua
@@ -1,3 +1,5 @@
+RedEM = exports["redem_roleplay"]:RedEM()
+
 -- Based on Malik's and Blue's animal shelters and vorp animal shelter --
 local data = {}
 local VorpCore = {}
@@ -20,14 +22,17 @@ AddEventHandler('rdn_companions:sellpet', function()
 	local _src = source
 
 	if Config.Framework == "redem" then
-	  TriggerEvent('redemrp:getPlayerFromId', _src, function(user)
+	  --TriggerEvent('redemrp:getPlayerFromId', _src, function(user)
+			local user = RedEM.GetPlayer(_src)
 			local u_identifier = user.getIdentifier()
 			local u_charid = user.getSessionVar("charid")
 
-			 MySQL.Sync.execute("DELETE FROM companions WHERE identifier = @identifier AND charidentifier = @charidentifier", {["identifier"] = u_identifier, ['charidentifier'] = u_charid})
+			exports.oxmysql:execute("DELETE FROM companions WHERE identifier = @identifier AND charidentifier = @charidentifier", {["identifier"] = u_identifier, ['charidentifier'] = u_charid})
+
+			--MySQL.Sync.execute("DELETE FROM companions WHERE identifier = @identifier AND charidentifier = @charidentifier", {["identifier"] = u_identifier, ['charidentifier'] = u_charid})
 			TriggerClientEvent('rdn_companions:removedog', _src)
 			
-		end)
+		--end)
 	elseif Config.Framework == "vorp" then
 			local Character = VorpCore.getUser(_src).getUsedCharacter
 			local u_identifier = Character.identifier
@@ -45,7 +50,8 @@ AddEventHandler('rdn_companions:feedPet', function(xp)
         local _src = source
 		
 		if Config.Framework == "redem" then
-	TriggerEvent('redemrp:getPlayerFromId', _src, function(user)
+	--TriggerEvent('redemrp:getPlayerFromId', _src, function(user)
+		local user = RedEM.GetPlayer(_src)
         local u_identifier = user.getIdentifier()
         local u_charid = user.getSessionVar("charid")
 		
@@ -59,9 +65,11 @@ AddEventHandler('rdn_companions:feedPet', function(xp)
 
 				ItemData.RemoveItem(1)
 
-				 local Parameters = { ['identifier'] = u_identifier, ['charidentifier'] = u_charid,  ['addedXp'] = Config.XpPerFeed }
-				MySQL.Sync.execute("UPDATE companions SET xp = xp + @addedXp  WHERE identifier = @identifier AND charidentifier = @charidentifier", Parameters, function(result) end)		
-				TriggerClientEvent('redem_roleplay:NotifyLeft',_src, "+"..Config.XpPerFeed.." Pet XP",newXp.."/"..Config.FullGrownXp.." XP" , "HUD_TOASTS", "toast_mp_status_change", 8000)
+				local Parameters = { ['identifier'] = u_identifier, ['charidentifier'] = u_charid,  ['addedXp'] = Config.XpPerFeed }
+				exports.oxmysql:execute("UPDATE companions SET xp = xp + @addedXp  WHERE identifier = @identifier AND charidentifier = @charidentifier", Parameters, function(result) end)
+
+				--MySQL.Sync.execute("UPDATE companions SET xp = xp + @addedXp  WHERE identifier = @identifier AND charidentifier = @charidentifier", Parameters, function(result) end)		
+				TriggerClientEvent('redem_roleplay:NotifyLeft',_src, "+"..Config.XpPerFeed.." Pet XP",newXp.."/"..Config.FullGrownXp.." XP" , "HUD_TOASTS", "toast_mp_status_change", 4000)
 				TriggerClientEvent('rdn_companions:UpdateDogFed', _src, newXp)
 			else
 			
@@ -70,9 +78,10 @@ AddEventHandler('rdn_companions:feedPet', function(xp)
 			TriggerClientEvent('rdn_companions:UpdateDogFed', _src, newXp)
 			end	
 		else
-		 TriggerClientEvent( 'UI:DrawNotification', _src, _U('NoFood') )
+		 	--TriggerClientEvent( 'UI:DrawNotification', _src, _U('NoFood') )
+			TriggerClientEvent('redem_roleplay:NotifyLeft',_src, _U('NoFood'), "", "menu_textures", "cross", 4000)
 		end
-	end)
+	--end)
 	elseif Config.Framework == 'vorp' then
 		local Character = VorpCore.getUser(_src).getUsedCharacter
 		local u_identifier = Character.identifier
@@ -101,113 +110,129 @@ AddEventHandler('rdn_companions:feedPet', function(xp)
 end)
 
 RegisterServerEvent('rdn_companions:buydog')
-AddEventHandler('rdn_companions:buydog', function (args)
-        local _src = source
-	if Config.Framework == "redem" then
-	TriggerEvent('redemrp:getPlayerFromId', _src, function(user)
-        local u_identifier = user.getIdentifier()
-        local u_charid = user.getSessionVar("charid")
-		local _price = args['Price']
-		local _model = args['Model']
-		local skin = math.floor(math.random(0, 2))
-		local canTrack = CanTrack(_src)
-		u_money = user.getMoney()
+AddEventHandler('rdn_companions:buydog', function(args)
+    local src = source
+    local userIdentifier, characterIdentifier, canTrack, money, price, model, skin
 
-		if u_money <= _price then
-			TriggerClientEvent( 'UI:DrawNotification', _src, _U('NoMoney') )
-			return
-		end
+    if Config.Framework == 'redem' then
+        local user = RedEM.GetPlayer(src)
+        userIdentifier = user.getIdentifier()
+        characterIdentifier = user.getSessionVar('charid')
+        money = user.getMoney()
+    elseif Config.Framework == 'vorp' then
+        local character = VorpCore.getUser(src).getUsedCharacter
+        userIdentifier = character.identifier
+        characterIdentifier = character.charIdentifier
+        money = character.money
+    end
 
-		MySQL.Async.fetchAll("SELECT * FROM companions WHERE identifier = @identifier AND charidentifier = @charidentifier", {['identifier'] = u_identifier, ['charidentifier'] = u_charid}, function(result)
-			if #result > 0 then 
-				local Parameters = { ['identifier'] = u_identifier, ['charidentifier'] = u_charid,  ['dog'] = _model, ['skin'] = skin , ['xp'] = 0 }
-			   MySQL.Sync.execute(" UPDATE companions SET dog = @dog, skin = @skin, xp = @xp WHERE identifier = @identifier AND charidentifier = @charidentifier", Parameters, function(r1)
-					user.removeMoney(_price)
-					TriggerClientEvent('rdn_companions:spawndog', _src, _model, skin, true, 0,canTrack)
-					TriggerClientEvent( 'UI:DrawNotification', _src, _U('ReplacePet') )
-				end)
-			else
-				local Parameters = { ['identifier'] = u_identifier, ['charidentifier'] = u_charid,  ['dog'] = _model, ['skin'] = skin, ['xp'] = 0 }
-			   MySQL.Sync.execute("INSERT INTO companions ( `identifier`,`charidentifier`,`dog`,`skin`, `xp` ) VALUES ( @identifier,@charidentifier, @dog, @skin, @xp )", Parameters, function(r2)
-					user.removeMoney(_price)
-					TriggerClientEvent('rdn_companions:spawndog', _src, _model, skin, true, 0,canTrack)
-					TriggerClientEvent( 'UI:DrawNotification', _src, _U('NewPet') )
-				end)
-			end
-		end)
-	end)
-	elseif Config.Framework == "vorp" then
-		local Character = VorpCore.getUser(_src).getUsedCharacter
-		local u_identifier = Character.identifier
-		local u_charid = Character.charIdentifier
-		local _price = args['Price']
-		local _model = args['Model']
-		local skin = math.floor(math.random(0, 2))
-		local canTrack = CanTrack(_src)
-		u_money = Character.money
+    canTrack = CanTrack(src)
+    price = args['Price']
+    model = args['Model']
+    skin = math.floor(math.random(0, 2))
 
-		if u_money <= _price then
-			TriggerClientEvent( 'UI:DrawNotification', _src, _U('NoMoney') )
-			return
-		end
-		exports.ghmattimysql:execute("SELECT * FROM companions WHERE identifier = @identifier AND charidentifier = @charidentifier", {['identifier'] = u_identifier, ['charidentifier'] = u_charid}, function(result)
-			if #result > 0 then 
-				local Parameters = { ['identifier'] = u_identifier, ['charidentifier'] = u_charid,  ['dog'] = _model, ['skin'] = skin , ['xp'] = 0 }
-			   exports.ghmattimysql:execute(" UPDATE companions SET dog = @dog, skin = @skin, xp = @xp WHERE identifier = @identifier AND charidentifier = @charidentifier", Parameters, function(r1)
-					Character.removeCurrency(0, _price)
-					TriggerClientEvent('rdn_companions:spawndog', _src, _model, skin, true, 0,canTrack)
-					TriggerClientEvent( 'UI:DrawNotification', _src, _U('ReplacePet') )
-				end)
-			else
-				local Parameters = { ['identifier'] = u_identifier, ['charidentifier'] = u_charid,  ['dog'] = _model, ['skin'] = skin, ['xp'] = 0 }
-			   exports.ghmattimysql:execute("INSERT INTO companions ( `identifier`,`charidentifier`,`dog`,`skin`, `xp` ) VALUES ( @identifier,@charidentifier, @dog, @skin, @xp )", Parameters, function(r2)
-					Character.removeCurrency(0, _price)
-					TriggerClientEvent('rdn_companions:spawndog', _src, _model, skin, true, 0,canTrack)
-					TriggerClientEvent( 'UI:DrawNotification', _src, _U('NewPet') )
-				end)
-			end
-		end)			
-	end
+    if money <= price then
+        --TriggerClientEvent('UI:DrawNotification', src, _U('NoMoney'))
+		TriggerClientEvent('redem_roleplay:NotifyLeft',src, _U('NoMoney'), "", "menu_textures", "cross", 4000)
+        return
+    end
+
+    local result = exports.oxmysql:fetchSync('SELECT * FROM companions WHERE identifier = @identifier AND charidentifier = @charidentifier', {
+        ['identifier'] = userIdentifier,
+        ['charidentifier'] = characterIdentifier
+    })
+
+    if #result > 0 then
+        local parameters = {
+            ['identifier'] = userIdentifier,
+            ['charidentifier'] = characterIdentifier,
+            ['dog'] = model,
+            ['skin'] = skin,
+            ['xp'] = 0
+        }
+
+        exports.oxmysql:execute('UPDATE companions SET dog = @dog, skin = @skin, xp = @xp WHERE identifier = @identifier AND charidentifier = @charidentifier', parameters, function(result)
+            if Config.Framework == 'redem' then
+				local user = RedEM.GetPlayer(src)
+                user.removeMoney(price)
+            elseif Config.Framework == 'vorp' then
+                character.removeCurrency(0, price)
+            end
+
+            TriggerClientEvent('rdn_companions:spawndog', src, model, skin, true, 0, canTrack)
+            --TriggerClientEvent('UI:DrawNotification', src, _U('ReplacePet'))
+			TriggerClientEvent('redem_roleplay:NotifyLeft',src, _U('ReplacePet'), "", "menu_textures", "cross", 4000)
+        end)
+    else
+        local parameters = {
+            ['identifier'] = userIdentifier,
+            ['charidentifier'] = characterIdentifier,
+            ['dog'] = model,
+            ['skin'] = skin,
+            ['xp'] = 0
+        }
+
+        exports.oxmysql:insert('INSERT INTO companions ( `identifier`,`charidentifier`,`dog`,`skin`, `xp` ) VALUES ( @identifier,@charidentifier, @dog, @skin, @xp )', parameters, function(result)
+            if Config.Framework == 'redem' then
+				local user = RedEM.GetPlayer(src)
+                user.removeMoney(price)
+            elseif Config.Framework == 'vorp' then
+                character.removeCurrency(0, price)
+            end
+
+            TriggerClientEvent('rdn_companions:spawndog', src, model, skin, true, 0, canTrack)
+            --TriggerClientEvent('UI:DrawNotification', src, _U('NewPet'))
+			TriggerClientEvent('redem_roleplay:NotifyLeft',src, _U('NewPet'), "", "menu_textures", "menu_icon_tick", 4000)
+        end)
+    end
 end)
 
 RegisterServerEvent('rdn_companions:loaddog')
 AddEventHandler('rdn_companions:loaddog', function()
-    local _src   = source
-	if Config.Framework == "redem" then
-	TriggerEvent('redemrp:getPlayerFromId', _src, function(user)
-		local u_identifier = user.getIdentifier()
-		local u_charid = user.getSessionVar("charid")
-		local canTrack = CanTrack(_src)
-		local Parameters = { ['identifier'] = u_identifier, ['charidentifier'] = u_charid }
-		MySQL.Async.fetchAll( "SELECT * FROM companions WHERE identifier = @identifier  AND charidentifier = @charidentifier", Parameters, function(result)
-			if result[1] then
-				local dog = result[1].dog
-				local skin = result[1].skin
-				local xp = result[1].xp or 0
-				TriggerClientEvent("rdn_companions:spawndog", _src, dog, skin, false, xp,canTrack)
-			else
-				TriggerClientEvent( 'UI:DrawNotification', _src, _U('NoPet') )
-			end
+	local src = source
+	local userIdentifier, characterIdentifier, canTrack
+
+	if Config.Framework == 'redem' then
+		userIdentifier = RedEM.GetPlayer(src).getIdentifier()
+		characterIdentifier = RedEM.GetPlayer(src).getSessionVar('charid')
+	elseif Config.Framework == 'vorp' then
+		local character = VorpCore.getUser(src).getUsedCharacter
+		userIdentifier = character.identifier
+		characterIdentifier = character.charIdentifier
+	end
+
+	canTrack = CanTrack(src)
+
+	local queryParameters = {
+		['identifier'] = userIdentifier,
+		['charidentifier'] = characterIdentifier,
+	}
+
+	if Config.Framework == 'redem' then
+		local result = exports.oxmysql:fetchSync('SELECT * FROM companions WHERE identifier = @identifier AND charidentifier = @charidentifier', queryParameters)
+
+		handleLoadDogResult(src, result, canTrack)
+		--exports.oxmysql:executeSync('SELECT * FROM companions WHERE identifier = @identifier AND charidentifier = @charidentifier', queryParameters, function(result)
+			--handleLoadDogResult(src, result, canTrack)
+		--end)
+	elseif Config.Framework == 'vorp' then
+		exports.ghmattimysql:execute('SELECT * FROM companions WHERE identifier = @identifier AND charidentifier = @charidentifier', queryParameters, function(result)
+			handleLoadDogResult(src, result, canTrack)
 		end)
-	end)
-	elseif Config.Framework == "vorp" then
-		local Character = VorpCore.getUser(_src).getUsedCharacter
-		local u_identifier = Character.identifier
-		local u_charid = Character.charIdentifier
-		local canTrack = CanTrack(_src)
-		local Parameters = { ['identifier'] = u_identifier, ['charidentifier'] = u_charid }
-		exports.ghmattimysql:execute( "SELECT * FROM companions WHERE identifier = @identifier  AND charidentifier = @charidentifier", Parameters, function(result)
-			if result[1] then
-				local dog = result[1].dog
-				local skin = result[1].skin
-				local xp = result[1].xp or 0
-				TriggerClientEvent("rdn_companions:spawndog", _src, dog, skin, false, xp,canTrack)
-			else
-				TriggerClientEvent( 'UI:DrawNotification', _src, _U('NoPet') )
-			end
-		end)	
 	end
 end)
+
+function handleLoadDogResult(src, result, canTrack)
+	if result[1] then
+		local dog = result[1].dog
+		local skin = result[1].skin
+		local xp = result[1].xp or 0
+		TriggerClientEvent('rdn_companions:spawndog', src, dog, skin, false, xp, canTrack)
+	else
+		--TriggerClientEvent('UI:DrawNotification', src, _U('NoPet'))
+		TriggerClientEvent('redem_roleplay:NotifyLeft',src, _U('NoPet'), "", "menu_textures", "cross", 4000)
+	end
+end
 
 
 function CanTrack(source)
@@ -232,9 +257,10 @@ function getJob(source)
  local cb = false
  
 	 if Config.Framework == "redem" then
-		TriggerEvent('redemrp:getPlayerFromId', source, function(user)
+		--TriggerEvent('redemrp:getPlayerFromId', source, function(user)
+			local user = RedEM.GetPlayer(source)
 			cb = user.getJob() 
-		end)	
+		--end)	
 	 elseif Config.Framework == "vorp" then
 		local Character = VorpCore.getUser(source).getUsedCharacter
 		cb = Character.job
